8c9ec112c0c8642cb9de837106da1cc4ac5d3497
VitalinaMak <vitamak1108@gmail.com>
2025-10-15 15:07:43 +0300

    saving the result to variable is added

--- Koodi.js
+++ Koodi.js
@@ -1,7 +1,24 @@
+"use strict";
+
+let kokonaisPisteet = 0;
+
 const lomake = document.getElementById("lomake");
-const vastaus = "";
+
 lomake.addEventListener("submit", function (event) {
     event.preventDefault();
-    vastaus = document.querySelector('input[name="unit"]:checked').value;
+    let vastaus = document.querySelector('input[name="unit"]:checked').value;  //haetaan vastaus lomakeesta
+    console.log(vastaus);
+
+    let vastaukenTeksti = document.querySelector('input[name="unit"]:checked').nextSibling;
+    if (vastaus=="0") {
+        vastaukenTeksti.className+="wrongAnswer";
+    } else {
+        vastaukenTeksti.className = "rightAnswer";
+    }
+
+    kokonaisPisteet += parseInt(vastaus);
+    console.log(kokonaisPisteet);
+
+
+    /* window.location.href = "tietovisa1.html"; */
 })
-console.log(vastaus);
